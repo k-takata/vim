ff6882c85b06103e73fafb88c9228cb98cfb81f4
K.Takata <kentkt@csc.jp>
2020-02-18 22:48:06 +0900

    Fix handling of backslash
    ```
    let x = '\x31'
    echo $"${x}"
    ```
    
    This should print "\x31" not "1".

--- src/eval.c
+++ src/eval.c
@@ -3846,6 +3846,11 @@ escape_quotes_in_quote(char_u *x, int is_literal_string)
 	    ga_concat(&result, (char_u *) "\\\"");
 	    continue;
 	}
+	else if (!is_literal_string && *p == '\\')
+	{
+	    ga_concat(&result, (char_u *) "\\\\");
+	    continue;
+	}
 	ga_concatn(&result, p, mb_ptr2len(p));
     }
 
